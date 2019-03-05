89f3a8309e7e491b8fdaf03686c68cb88ce52a75
Xenossolitarius <ipesic@fesb.hr>
2019-03-05 19:23:39 +0100

    Added spinner

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+
+    <transition name="fade">
+    <Spinner v-if="spinIt" />
+    </transition>
+
     <SideMenu />
     <LoadWindow />
   </div>
@@ -9,12 +14,29 @@
 
 import SideMenu from './components/layouts/SideMenu.vue'
 import LoadWindow from './components/LoadWindow.vue'
+import Spinner from './components/layouts/Spinner.vue'
+import { setTimeout } from 'timers';
 
 export default {
   name: 'app',
   components: {
     SideMenu,
-    LoadWindow
+    LoadWindow,
+    Spinner
+  },
+  data(){
+    return {
+      spinIt: true
+    }
+  },
+  methods:{
+
+  },
+  mounted(){
+    setTimeout(()=>{
+      this.spinIt= false;
+      
+    },3000);
   }
 }
 </script>
@@ -27,5 +49,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 1;
 }
 </style>
---
+++ src/components/layouts/Spinner.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="rizk-loader" class="rizk-loader loading" style=""><img src="../../assets/wor-loader-icon.png" alt="Rizk loading" class="loader-wheel"></div>
+</template>
+
+<script>
+export default {
+    name:'Spinner'
+}
+</script>
+
+<style  scoped>
+.loading:before {
+  content: '';
+  display: block;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgb(0, 0, 0);
+}
+
+</style>
+
